ed36a63295af4ae16ef30191e216e18a7a28fb56
Sukera <Seelengrab@users.noreply.github.com>
2019-12-14 21:37:59 +0100

    ADDED day 13 & 14

--- 2019/13/13.jl
+++ 2019/13/13.jl
@@ -1,8 +1,63 @@
-if length(ARGS) != 1
-    println("Need exactly one argument!")
-    exit(1)
+const empty = 0
+const wall = 1
+const block = 2
+const hpaddle = 3
+const ball = 4
+
+function updateMap!(out, tiles, score)
+    x = take!(out) + 1
+    y = take!(out) + 1
+    id = take!(out)
+    if x == 0 && y == 1
+        score[] = id
+    else
+        if id == wall
+            @show x,y,id
+        end
+        tiles[x,y] = id
+    end
 end
 
-function parseInput()
+function drawMap(tiles, score)
+    buf = IOBuffer()
+    print(buf, b"^L")
+    println(buf, score[])
+    for y in axes(tiles, 2)
+        for x in axes(tiles, 1)
+            if tiles[x,y] == 0
+                print(buf, '.')
+            else
+                print(buf, tiles[x,y])
+            end
+        end
+        println(buf, "")
+    end
+    println(stdout, String(take!(buf)))
+end
 
+function runGame(inp)
+    inComm = InfChannel{Int}(0)
+    outComm = Channel{Int}(Inf)
+    diagComm = Channel{Int}(Inf)
+    m = Intmachine(0, deepcopy(inp), false, inComm=inComm, outComm=outComm, diagComm=diagComm)
+    t = @async run!(m)
+
+    tiles = zeros(Int, 42,20)
+    score = Ref(0)
+    x = -1
+    y = -1
+
+    # initial drawing
+    for _ in 1:(42*20)
+        updateMap!(outComm, tiles, score)
+    end
+    drawMap(tiles, score)
+
+    while !istaskdone(t) || isready(outComm)
+        updateMap!(outComm, tiles, score)
+        drawMap(tiles, score)
+    end
+
+    println(score[])
+    return tiles, score[]
 end
--- 2019/14/14.jl
+++ 2019/14/14.jl
@@ -1,8 +1,54 @@
-if length(ARGS) != 1
-    println("Need exactly one argument!")
-    exit(1)
+function parseInput(inp)
+    lines = split.(readlines(inp), " => ")
+    res = Dict{String,Tuple{Int,Dict{String,Int}}}()
+    for l in lines
+        quant, name = split(l[2], ' ')
+        req = split(l[1], ", ")
+        reqs = Dict(map(x -> x[2] => parse(Int,x[1]), split.(req, ' ')))
+        res[name] = (parse(Int,quant), reqs)
+    end
+
+    res
 end
 
-function parseInput()
+findNumOre(recs, numFuel=1) = findNumOre(Dict("FUEL" => numFuel), Dict{String, Int}(), recs)
 
+function findNumOre(needed, have, recipe)
+    if length(collect(keys(needed))) == 1 && haskey(needed, "ORE")
+        return needed["ORE"]
+    end
+
+    # println("--------")
+    # @show needed
+    # @show have
+    # println()
+    for (chem, quant) in needed
+        chem == "ORE" && continue
+        # @show chem, quant
+        delete!(needed, chem)
+
+        q, ingredients = recipe[chem]
+        if get!(have, chem, 0) < quant
+            while have[chem] < quant
+                have[chem] += q
+
+                for (c,iq) in ingredients
+                    needed[c] = get!(needed, c, 0) + iq - get!(have, c, 0)
+                    have[c] = 0
+                end
+            end
+        end
+
+        have[chem] -= quant
+    end
+
+    return findNumOre(needed, have, recipe)
+end
+
+function findMaxFuel(recs, x1=10_000, x2=100_000, numOre=1_000_000_000_000)
+    y1 = findNumOre(recs, x1)
+    y2 = findNumOre(recs, x2) # this one takes a little while
+    delta = (y2-y1)/(x2 - x1)
+    x = y1 - delta*x1
+    ceil((numOre - x) / delta) |> Int
 end
